cc1279032c7286db3604962e3ca993d54d1db626
Arham Siddiqui <66856134+arham-siddiqui@users.noreply.github.com>
2024-07-19 11:07:44 -0700

    Update closest_match.py

--- closest_match.py
+++ closest_match.py
@@ -4,12 +4,12 @@ def find_min_distance(d, database, prob_threshold, cosine_threshold, probability
     if (probability>prob_thresh):
         
         #find the minimum cosine distance
-        for i in range(len(database)):
-            distances.append(cosine_distance(d, list(database.values())[i]))
+        for i in database:
+            distances.append(cosine_distance(d, i.descriptor))
         minimum_distance = min(distances)
     
         if minimum_distance <= cosine_threshold:
-            return list(database.values())[distances.index(minimum_distance)]
+            return database[distances.index(minimum_distance)].name
         
         return "Person is not found"
         
